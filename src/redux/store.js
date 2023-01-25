09fd3dc92755b2282c938f06dabfbd915db6d9d6
camilevahviraki <camilevahviraki@gmail.com>
2023-01-25 02:20:22 +0200

    Update redux/store

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,9 +2,11 @@ import { applyMiddleware, combineReducers, createStore } from 'redux';
 import thunk from './logger/logger';
 import logger from './logger/thunk';
 import createStoresReducer from './stores/createStoreReducer';
+import getStoresReducer from './stores/getStoresReducer';
 
 const rootReducer = combineReducers({
   createStoresReducer,
+  getStoresReducer,
 });
 
 const store = createStore(
