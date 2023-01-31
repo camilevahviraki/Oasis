9966424ea33825b4083c85a8684ae861250d121c
camilevahviraki <camilevahviraki@gmail.com>
2023-02-01 00:57:38 +0200

    Add current user credentials in MainRouter

--- src/router/MainRouter.js
+++ src/router/MainRouter.js
@@ -1,14 +1,31 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 import SellerRouter from './SellerRouter';
 import ClientRouter from './ClientRouter';
+import AuthRoutes from '../authentication/AuthRoutes';
+import UserRoutes from '../user_account/userRoutes';
 import Header from '../header/Header';
 
 const MainRouter = () => {
+
+  const user = useSelector(state => state.authenticationReducer);
+
   return (
     <BrowserRouter>
-      <Header/>
-      <SellerRouter/>
+       <AuthRoutes/>
+      {
+        user.token? 
+        <>
+          <Header/>
+          <SellerRouter/>
+          <UserRoutes/>
+        </>:
+        <></>
+      }
+      
+      
+      
     </BrowserRouter>
   )
 }
