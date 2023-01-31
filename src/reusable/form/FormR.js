ee67ee656a61fe4a0b2a101aa4141c704d130d42
camilevahviraki <camilevahviraki@gmail.com>
2023-02-01 00:56:49 +0200

    Update reusable form

--- src/reusable/form/FormR.js
+++ src/reusable/form/FormR.js
@@ -8,11 +8,13 @@ const FormR = (props) => {
     submitFunction,
     submitButton,
     submitClass,
+    errorMessage,
+    inputErrorArr,
   } = props;
     
   return (
     <form className={classForm} onSubmit={submitFunction}>
-        {inputsArray.map((inputObj) => (<>
+        {inputsArray.map((inputObj, key) => (<>
           {
             inputObj.type === 'textarea'?
             (
@@ -24,15 +26,23 @@ const FormR = (props) => {
               </textarea>
             ):
             (
+              <div className='formR-input-wrap'>
               <input
               type={inputObj.type}
               name={inputObj.name}
               className={inputObj.classInput? inputObj.classInput : 'input'}
               placeholder={inputObj.placeholder}
               />
+            {
+              inputErrorArr && inputErrorArr[key] === 1?(<div className='error-input-show'></div>)
+              : 
+              null
+            }
+              </div>
             )
           }
           </>))}
+          <p>{errorMessage}</p>
           <button type="submit" className={submitClass}>{submitButton}</button>    
     </form>
   )
