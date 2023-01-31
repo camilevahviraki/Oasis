685b4c7d98f4e20303168670ed635db5889f8154
camilevahviraki <camilevahviraki@gmail.com>
2023-02-01 00:58:11 +0200

    Update seller router

--- src/router/SellerRouter.js
+++ src/router/SellerRouter.js
@@ -1,13 +1,24 @@
-import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import React, { useState } from 'react';
+import { useSelector } from 'react-redux';
+import { Routes, Route } from 'react-router-dom';
 import CreateStore from '../seller/stores/CreateStore/CreateStore';
 import MyStores from '../seller/stores/MyStores/MyStores';
+import StoreShow from '../seller/stores/StoreDetails/StoreShow';
 
 const SellerRouter = () => {
+
+  // const [storeLink, setStoreLink] = useState(null);
+  const storeLink = useSelector((state) => state.storeLinkReducer);
+  // console.log(storeLinkSaved.link)
+
   return (
       <Routes>
          <Route path='create-store' element={(<CreateStore/>)}/>
          <Route path='my-stores' element={(<MyStores/>)}/>
+         <Route path={storeLink.link?
+          `my-stores/${storeLink.link.link}`:
+          `my-stores/error_page`
+        } element={(<StoreShow/>)}/>
       </Routes>
   )
 }
