11a66b17cedad1dde7f7a27f2b87fa9882760648
camilevahviraki <camilevahviraki@gmail.com>
2023-02-01 00:54:36 +0200

    Update redux store

--- src/redux/store.js
+++ src/redux/store.js
@@ -3,10 +3,16 @@ import thunk from './logger/logger';
 import logger from './logger/thunk';
 import createStoresReducer from './stores/createStoreReducer';
 import getStoresReducer from './stores/getStoresReducer';
+import authenticationReducer from './authentication/signUpReducer';
+import storeLinkReducer from './storeLink/storeLinkReducer';
+import getStoreShowReducer from './stores/getStoreShowReducer';
 
 const rootReducer = combineReducers({
   createStoresReducer,
   getStoresReducer,
+  authenticationReducer,
+  storeLinkReducer,
+  getStoreShowReducer,
 });
 
 const store = createStore(
