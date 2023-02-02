e5420b5f28f422ffab59c5547ae96fe8ae8b4457
camilevahviraki <camilevahviraki@gmail.com>
2023-02-02 23:49:01 +0200

    Update reusable form

--- src/reusable/form/FormR.js
+++ src/reusable/form/FormR.js
@@ -10,6 +10,7 @@ const FormR = (props) => {
     submitClass,
     errorMessage,
     inputErrorArr,
+    inputWrapperClassName,
   } = props;
     
   return (
@@ -18,18 +19,34 @@ const FormR = (props) => {
           {
             inputObj.type === 'textarea'?
             (
-              <textarea
-                name={inputObj.name}
-                className={inputObj.classInput? inputObj.classInput : 'textarea'}
-                placeholder={inputObj.placeholder}
-              >
-              </textarea>
+              <div className={inputWrapperClassName? inputWrapperClassName :'formR-input-wrap'}>
+                {
+                  inputObj.label?(<label htmlFor={inputObj.name}>{inputObj.label}</label>):(<></>)
+                }
+                <textarea
+                  id={inputObj.name}
+                  name={inputObj.name}
+                  className={inputObj.classInput? inputObj.classInput : 'textarea'}
+                  placeholder={inputObj.placeholder}
+                >
+                </textarea>
+                {inputErrorArr && inputErrorArr[key] === 1?(
+              <div className='error-input-show'></div>)
+              : 
+              null
+            }
+
+              </div>              
             ):
             (
-              <div className='formR-input-wrap'>
+              <div className={inputWrapperClassName? inputWrapperClassName :'formR-input-wrap'}>
+              {
+                inputObj.label?(<label htmlFor={inputObj.name}>{inputObj.label}</label>):(<></>)
+              }
               <input
               type={inputObj.type}
               name={inputObj.name}
+              id={inputObj.name}
               className={inputObj.classInput? inputObj.classInput : 'input'}
               placeholder={inputObj.placeholder}
               />
