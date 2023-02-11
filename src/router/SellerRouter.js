128f10e7d9ddaf26ec2a9eff6af1f1eb0abf3b28
camilevahviraki <camilevahviraki@gmail.com>
2023-02-12 01:30:52 +0200

    update router folder

--- src/router/SellerRouter.js
+++ src/router/SellerRouter.js
@@ -3,7 +3,9 @@ import { useSelector } from 'react-redux';
 import { Routes, Route } from 'react-router-dom';
 import CreateStore from '../seller/stores/CreateStore/CreateStore';
 import MyStores from '../seller/stores/MyStores/MyStores';
-import StoreShow from '../seller/stores/StoreDetails/StoreShow';
+import StoreEdit from '../seller/stores/EditStore/EditStore';
+import StoreItems from '../seller/stores/StoreDetails/StoreItems';
+import CreateItem from '../seller/items/createItem/createItem';
 
 const SellerRouter = () => {
 
@@ -13,12 +15,23 @@ const SellerRouter = () => {
 
   return (
       <Routes>
-         <Route path='create-store' element={(<CreateStore/>)}/>
-         <Route path='my-stores' element={(<MyStores/>)}/>
-         <Route path={storeLink.link?
-          `my-stores/${storeLink.link.link}`:
-          `my-stores/error_page`
-        } element={(<StoreShow/>)}/>
+          <Route path='create-store' element={(<CreateStore/>)}/>
+          <Route path='my-stores' element={(<MyStores/>)}/>
+          <Route path={storeLink.link?
+            `my-stores/${storeLink.link.link}/edit`:
+            `my-stores/error_page`
+            } element={(<StoreEdit/>)}
+          />
+          <Route path={storeLink.link?
+              `my-stores/${storeLink.link.link}/items`:
+              `my-stores/error_page`
+            } element={(<StoreItems/>)}
+          />
+          <Route path={storeLink.link?
+              `my-stores/${storeLink.link.link}/item/new`:
+              `my-stores/error_page`
+            } element={(<CreateItem/>)}
+          />
       </Routes>
   )
 }
