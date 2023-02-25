9a4ba9ce1e7867d875c8ef2a9e53aa1de607a39d
camilevahviraki <camilevahviraki@gmail.com>
2023-02-25 17:26:32 +0200

    Add reusable form

--- src/reusable/form/FormR.js
+++ src/reusable/form/FormR.js
@@ -1,7 +1,7 @@
-import React from 'react'
+import React from "react";
+import Countries from "../../components/countries/countries";
 
 const FormR = (props) => {
-
   const {
     classForm,
     inputsArray,
@@ -11,58 +11,90 @@ const FormR = (props) => {
     errorMessage,
     inputErrorArr,
     inputWrapperClassName,
+    getSelectedCountry,
   } = props;
-    
+
   return (
     <form className={classForm} onSubmit={submitFunction} id={classForm}>
-        {inputsArray.map((inputObj, key) => (<>
-          {
-            inputObj.type === 'textarea'?
-            (
-              <div className={inputWrapperClassName? inputWrapperClassName :'formR-input-wrap'}>
-                {
-                  inputObj.label?(<label htmlFor={inputObj.name}>{inputObj.label}</label>):(<></>)
-                }
-                <textarea
-                  id={inputObj.name}
-                  name={inputObj.name}
-                  className={inputObj.classInput? inputObj.classInput : 'textarea'}
-                  placeholder={inputObj.placeholder}
-                >
-                </textarea>
-                {inputErrorArr && inputErrorArr[key] === 1?(
-              <div className='error-input-show'></div>)
-              : 
-              null
-            }
-
-              </div>              
-            ):
-            (
-              <div className={inputWrapperClassName? inputWrapperClassName :'formR-input-wrap'}>
-              {
-                inputObj.label?(<label htmlFor={inputObj.name}>{inputObj.label}</label>):(<></>)
-              }
-              <input
-              type={inputObj.type}
-              name={inputObj.name}
-              id={inputObj.name}
-              className={inputObj.classInput? inputObj.classInput : 'input'}
-              placeholder={inputObj.placeholder}
-              />
+      {inputsArray.map((inputObj, key) => (
+        <>
+          {inputObj.type === "select-country" ? (
+            <>
+            <Countries
+              data={inputObj.data}
+              inputClass={inputObj.classInput}
+              inputLabel={inputObj.label? inputObj.label : null}
+              getSelectedCountry={getSelectedCountry}
+            />
             {
-              inputErrorArr && inputErrorArr[key] === 1?(<div className='error-input-show'></div>)
-              : 
-              null
-            }
-              </div>
-            )
-          }
-          </>))}
-          <p>{errorMessage}</p>
-          <button type="submit" className={submitClass}>{submitButton}</button>    
+              inputErrorArr && inputErrorArr[key] === 1 ? (
+              <div className="error-input-show"/>
+            ) : null}
+          </>
+          ) : (
+            <>
+              {inputObj.type === "textarea" ? (
+                <div
+                  className={
+                    inputWrapperClassName
+                      ? inputWrapperClassName
+                      : "formR-input-wrap"
+                  }
+                >
+                  {inputObj.label ? (
+                    <label htmlFor={inputObj.name}>{inputObj.label}</label>
+                  ) : (
+                    <></>
+                  )}
+                  <textarea
+                    id={inputObj.name}
+                    name={inputObj.name}
+                    className={
+                      inputObj.classInput ? inputObj.classInput : "textarea"
+                    }
+                    placeholder={inputObj.placeholder}
+                  ></textarea>
+                  {inputErrorArr && inputErrorArr[key] === 1 ? (
+                    <div className="error-input-show"></div>
+                  ) : null}
+                </div>
+              ) : (
+                <div
+                  className={
+                    inputWrapperClassName
+                      ? inputWrapperClassName
+                      : "formR-input-wrap"
+                  }
+                >
+                  {inputObj.label ? (
+                    <label htmlFor={inputObj.name}>{inputObj.label}</label>
+                  ) : (
+                    <></>
+                  )}
+                  <input
+                    type={inputObj.type}
+                    name={inputObj.name}
+                    id={inputObj.name}
+                    className={
+                      inputObj.classInput ? inputObj.classInput : "input"
+                    }
+                    placeholder={inputObj.placeholder}
+                  />
+                  {inputErrorArr && inputErrorArr[key] === 1 ? (
+                    <div className="error-input-show"></div>
+                  ) : null}
+                </div>
+              )}
+            </>
+          )}
+        </>
+      ))}
+      <p>{errorMessage}</p>
+      <button type="submit" className={submitClass}>
+        {submitButton}
+      </button>
     </form>
-  )
-}
+  );
+};
 
-export default FormR
+export default FormR;
