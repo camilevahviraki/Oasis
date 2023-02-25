70dc852b6183a1642b9b3fbeedeb719350e13725
camilevahviraki <camilevahviraki@gmail.com>
2023-02-25 17:18:14 +0200

    Add seller add Item show

---
+++ src/seller/items/itemList/ItemIndex.js
@@ -0,0 +1,59 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { setItemLink } from "../../../redux/itemLink/itemLinkreducer";
+import linkName from "../../../reusable/remove-blanck-space/linkName";
+import LimitText from "../../../reusable/limit-text-length/limitText";
+import ImageSilder from "../../../reusable/images_slider/ImageSilder";
+import './ItemIndex.css';
+
+const ItemsList = (props) => {
+  const dispatch = useDispatch();
+
+  const { itemsData, storeData , rows} = props;
+
+  const setStoreItemLink = (itemName, id) => {
+    let itemLink = null;
+    if (storeData) {
+      itemLink = `store/${linkName(storeData.name)}/item/${linkName(itemName)}`;
+    } else {
+      itemLink = `item/${linkName(itemName)}`;
+    }
+
+    dispatch(setItemLink(itemLink, id));
+  };
+
+  return (
+    <div className="store-items-list">
+      {itemsData.map((item) => {
+        const { items_images, mainName, names, price, store_id, description, id } = item;
+        console.log(item);
+        return (
+          <Link
+            to={
+              storeData
+                ? `../store/${linkName(storeData.name)}/item/${linkName(
+                    item.mainName
+                  )}`
+                : `../item/${linkName(item.mainName)}`
+            }
+            onClick={() => setStoreItemLink(item.mainName, id)}
+          >
+            <div className="store-item-wrapp">
+              <div className="store-item-image-wrapp">
+                <ImageSilder imagesArray={items_images} freeze={true} />
+              </div>
+              <div className="store-item-description">
+                <h4 className="store-item-mainName">{mainName}</h4>
+                <p className="store-item-price">${price}</p>
+                <LimitText text={description} limit={30} className={'store-item-description-text'}/>
+              </div>
+            </div>
+          </Link>
+        );
+      })}
+    </div>
+  );
+};
+
+export default ItemsList;
