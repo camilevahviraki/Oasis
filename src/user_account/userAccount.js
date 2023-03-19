bfba2131d9ded05e949a32c20052b520d7feffcc
camilevahviraki <camilevahviraki@gmail.com>
2023-03-19 14:33:18 +0200

    Update user account components

--- src/user_account/userAccount.js
+++ src/user_account/userAccount.js
@@ -1,64 +1,73 @@
 import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import {Link} from 'react-router-dom';
-import {userLogout} from '../redux/authentication/signUpReducer';
+import { Link } from 'react-router-dom';
+import { userLogout } from '../redux/authentication/signUpReducer';
 import CheckValidImage from '../reusable/check-image/checkValidImage';
 import defaultAvatar from '../images/user-show-icon.png';
-import './userAccount.css'
+import './userAccount.css';
 
 const UserAccount = () => {
-
   const dispatch = useDispatch();
-  const userData = useSelector(state => state.authenticationReducer); 
+  const userData = useSelector((state) => state.authenticationReducer);
 
-  const user = userData.user;
-  const userNames = user.first_name + '-' + user.last_name;
+  const { user } = userData;
+  const userNames = `${user.first_name}-${user.last_name}`;
 
   const {
-     first_name,
-     last_name,
-     avatar_url,
-     email,
-     updated_at,
-     id,
+    first_name,
+    last_name,
+    avatar_url,
+    email,
+    updated_at,
+    id,
   } = user;
 
   const onLogout = () => {
     dispatch(userLogout());
-  }
+  };
 
   return (
-    <div className='w-full flex flex-col center'>
-        <div className='logout-button-wrap'>
-          <button
-            type='button'
-            className='logout-button'
-            onClick={onLogout} 
-          >
-            LogOut
-          </button>
-        </div>
-        <div className='user-account-avatar-wrap'>
-          <img
-            src={CheckValidImage({avartarUrl: avatar_url,defaultImg: defaultAvatar})}
-            alt=''
-            className='user-account-avatar'
-            style={{ backgroundImage: `url(${avatar_url})`}}
-          />
-        </div>
-        <div className='user-account-edit-image'>
-           <Link to={`../${userNames}/edit-image`}>Edit image</Link>
-        </div>
-        <div className='user-account-details'>
-          <Link to={`../${userNames}/edit-names`}>
-            <h4 className='user-account-name'>{first_name}{' '}{last_name}</h4>
-          </Link>
-          <p>Email: <span>{email}</span></p>
-          <p>Last Upated at: <span>{updated_at}</span></p>
-        </div>
+    <div className="w-full flex flex-col center">
+      <div className="logout-button-wrap">
+        <button
+          type="button"
+          className="logout-button"
+          onClick={onLogout}
+        >
+          LogOut
+        </button>
+      </div>
+      <div className="user-account-avatar-wrap">
+        <img
+          src={CheckValidImage({ avartarUrl: avatar_url, defaultImg: defaultAvatar })}
+          alt=""
+          className="user-account-avatar"
+          style={{ backgroundImage: `url(${avatar_url})` }}
+        />
+      </div>
+      <div className="user-account-edit-image">
+        <Link to={`../${userNames}/edit-image`}>Edit image</Link>
+      </div>
+      <div className="user-account-details">
+        <Link to={`../${userNames}/edit-names`}>
+          <h4 className="user-account-name">
+            {first_name}
+            {' '}
+            {last_name}
+          </h4>
+        </Link>
+        <p>
+          Email:
+          <span>{email}</span>
+        </p>
+        <p>
+          Last Upated at:
+          <span>{updated_at}</span>
+        </p>
+      </div>
 
     </div>
-  )
-}
+  );
+};
 
 export default UserAccount;
--- src/user_account/userRoutes.js
+++ src/user_account/userRoutes.js
@@ -6,16 +6,15 @@ import UserAccount from './userAccount';
 import UserEditAccount from './userEditAccount';
 
 const UserRoutes = () => {
+  const userData = useSelector((state) => state.authenticationReducer);
+  const userNames = `${userData.user.first_name}-${userData.user.last_name}`;
 
-  const userData = useSelector(state => state.authenticationReducer);
-  const userNames = userData.user.first_name + '-' + userData.user.last_name;
-    
   return (
     <Routes>
-        <Route path={`account/${linkName(userNames)}`} element={(<UserAccount/>)}/>
-        <Route path={`${userNames}/account-edit`} element={(<UserEditAccount/>)}/>
+      <Route path={`account/${linkName(userNames)}`} element={(<UserAccount />)} />
+      <Route path={`${userNames}/account-edit`} element={(<UserEditAccount />)} />
     </Routes>
-  )
-}
+  );
+};
 
 export default UserRoutes;
