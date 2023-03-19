28b636c55d9be93071f9a8e6a3296e320fa139c4
camilevahviraki <camilevahviraki@gmail.com>
2023-03-19 14:26:50 +0200

    Update store link

--- src/redux/storeLink/storeLinkReducer.js
+++ src/redux/storeLink/storeLinkReducer.js
@@ -1,25 +1,26 @@
-const SET_STORE_LINK = "/redux/authentication/SET_STORE_LINK";
+const SET_STORE_LINK = '/redux/authentication/SET_STORE_LINK';
 
 const storeLinkReducer = (
-    state = {link: null}, action ) => {
-    switch (action.type) {
-      case SET_STORE_LINK: {
-        return action.data;
-      }default: {
-        const link = JSON.parse(localStorage.getItem('storeLink'));
-        if(link){
-            return {link: link};
-        }
-        return state;
+  state = { link: null }, action,
+) => {
+  switch (action.type) {
+    case SET_STORE_LINK: {
+      return action.data;
+    } default: {
+      const link = JSON.parse(localStorage.getItem('storeLink'));
+      if (link) {
+        return { link };
       }
+      return state;
     }
-}
+  }
+};
 
 export const setStoreLink = (link) => (
-    {
-        type: SET_STORE_LINK,
-        data: {link}
-    }
-)
+  {
+    type: SET_STORE_LINK,
+    data: { link },
+  }
+);
 
 export default storeLinkReducer;
