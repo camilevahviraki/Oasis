90b06a18dc76708fadb346bc4491bbb6ec1641f3
camilevahviraki <camilevahviraki@gmail.com>
2023-03-19 14:23:29 +0200

    Update currencies reducer

--- src/redux/currencies/currenciesReducer.js
+++ src/redux/currencies/currenciesReducer.js
@@ -1,27 +1,27 @@
-import linkURL from "../link";
-import axios from "axios";
+import axios from 'axios';
+import linkURL from '../link';
 
 const GET_CURRENCIES = 'redux/store/getStoresReducer/GET_CURRENCIES';
 
-
 const currenciesReducer = (state = [], action) => {
-    switch (action.type) {
-        case GET_CURRENCIES: {
-           return action.data;
-        }default:
-            return state
-    }
-} 
+  switch (action.type) {
+    case GET_CURRENCIES: {
+      return action.data;
+    } default:
+      return state;
+  }
+};
 
 export const getCurrencies = (token) => (dispatch) => {
-    axios.get(`${linkURL}/currencies`)
-      .then((response) => {dispatch(
+  axios.get(`${linkURL}/currencies`)
+    .then((response) => {
+      dispatch(
         {
           type: GET_CURRENCIES,
           data: response.data,
         },
-      )
+      );
     });
-}
+};
 
 export default currenciesReducer;
