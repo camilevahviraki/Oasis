552136148fdec99351701735a250e7d3b36422ba
camilevahviraki <camilevahviraki@gmail.com>
2023-03-19 14:17:01 +0200

    Update Authenbtification

--- src/authentication/forgotPassword.js
+++ src/authentication/forgotPassword.js
@@ -1,36 +1,35 @@
-import React, {useState} from 'react';
-import {Link} from 'react-router-dom';
-import {useDispatch} from 'react-redux';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
 import { userResetPassword } from '../redux/authentication/signUpReducer';
 import FormR from '../reusable/form/FormR';
 
 const ForgotPassword = () => {
-
   const dispatch = useDispatch();
 
   const [message, setMessage] = useState(null);
 
   const inputsArray = [
     {
-      type: "mail",
-      name: "email",
-      classInput: "user-authentication-form-input",
-      placeholder: "Email",
-    }
+      type: 'mail',
+      name: 'email',
+      classInput: 'user-authentication-form-input',
+      placeholder: 'Email',
+    },
   ];
 
   const onSignup = (e) => {
     e.preventDefault();
     const email = e.target.email.value;
 
-    if(!/\S+@\S+\.\S+/.test(email)){
-       setMessage('Please! Enter a valid email')
-    }else {
-      let formData = {
+    if (!/\S+@\S+\.\S+/.test(email)) {
+      setMessage('Please! Enter a valid email');
+    } else {
+      const formData = {
         user: {
-          email: email,
+          email,
         },
-        commit: "Send me reset password instructions",
+        commit: 'Send me reset password instructions',
       };
 
       setMessage(null);
@@ -39,31 +38,33 @@ const ForgotPassword = () => {
       console.log(formData);
     }
 
-  // const toChangePasswoed = {user: 
-  //   {reset_password_token: "[FILTERED]",
-  //    password: "[FILTERED]",
-  //    password_confirmation: "[FILTERED]"
-  //   }
-  //   }
-    
+    // const toChangePasswoed = {user:
+    //   {reset_password_token: "[FILTERED]",
+    //    password: "[FILTERED]",
+    //    password_confirmation: "[FILTERED]"
+    //   }
+    //   }
   };
 
   return (
     <div className="w-full flex flex-col items-center">
-      <h2 className='authentication-title'>Reset Password</h2>
+      <h2 className="authentication-title">Reset Password</h2>
       <FormR
-        classForm={"user-authentication-form "}
+        classForm="user-authentication-form "
         inputsArray={inputsArray}
         submitFunction={onSignup}
-        submitButton={"Send Me Reset Instructions"}
-        submitClass={"user-authentication-form-button"}
+        submitButton="Send Me Reset Instructions"
+        submitClass="user-authentication-form-button"
         errorMessage={message}
       />
 
-      <p>Back to login page? <Link to='../login'>Sign In</Link></p>
+      <p>
+        Back to login page?
+        <Link to="../login">Sign In</Link>
+      </p>
 
     </div>
-  )
-}
+  );
+};
 
 export default ForgotPassword;
--- src/authentication/updateAccount.js
+++ src/authentication/updateAccount.js
@@ -1,50 +1,49 @@
-import React, {useState} from 'react';
-import {Link} from 'react-router-dom';
-import FormR from "../reusable/form/FormR";
-import "./authentication.css";
-import userIcon from "../images/user-show-icon.png";
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import FormR from '../reusable/form/FormR';
+import './authentication.css';
+import userIcon from '../images/user-show-icon.png';
 
 const UpdateAccount = (props) => {
-
   const [useImage, setUserImage] = useState(userIcon);
   const [profilePopUp, setProfilePopUp] = useState(false);
   const [userAvatarFile, setUserAvatarFile] = useState(null);
   const [message, setMessage] = useState(null);
-  const [inputErrorArr, setInputErrorArr] = useState([0,0,0,0,0]);
+  const [inputErrorArr, setInputErrorArr] = useState([0, 0, 0, 0, 0]);
 
   const inputFirstName = [{
-    type: "text",
-    name: "first_name",
-    classInput: "user-authentication-form-input",
-    placeholder: "First name",
+    type: 'text',
+    name: 'first_name',
+    classInput: 'user-authentication-form-input',
+    placeholder: 'First name',
   }];
 
   const inputLastName = [{
-      type: "text",
-      name: "last_name",
-      classInput: "user-authentication-form-input",
-      placeholder: "Last name",
-    }];
+    type: 'text',
+    name: 'last_name',
+    classInput: 'user-authentication-form-input',
+    placeholder: 'Last name',
+  }];
 
   const inputEmail = [{
-    type: "mail",
-    name: "email",
-    classInput: "user-authentication-form-input",
-    placeholder: "Email",
-  }]  
+    type: 'mail',
+    name: 'email',
+    classInput: 'user-authentication-form-input',
+    placeholder: 'Email',
+  }];
 
   const inputPassword = [
     {
-      type: "password",
-      name: "password",
-      classInput: "user-authentication-form-input",
-      placeholder: "Password",
+      type: 'password',
+      name: 'password',
+      classInput: 'user-authentication-form-input',
+      placeholder: 'Password',
     },
     {
-      type: "password",
-      name: "confirm_password",
-      classInput: "user-authentication-form-input",
-      placeholder: "Confirm Password",
+      type: 'password',
+      name: 'confirm_password',
+      classInput: 'user-authentication-form-input',
+      placeholder: 'Confirm Password',
     },
   ];
 
@@ -64,33 +63,33 @@ const UpdateAccount = (props) => {
 
     if (firstName.length === 0 || lastName.length === 0 || email.length === 0 || password.length === 0) {
       setMessage('Please! These marked fields are required');
-      setInputErrorArr([1,1,1,1,1]);
-    }else if(!/\S+@\S+\.\S+/.test(email)){
+      setInputErrorArr([1, 1, 1, 1, 1]);
+    } else if (!/\S+@\S+\.\S+/.test(email)) {
       setMessage('Please! Enter a valid email');
-      setInputErrorArr([0, 0, 1, 0, 0 ]);
-    }else if(password !== confirmPassword){
-      setMessage('Password confirmation do not Match')
-      setInputErrorArr([0, 0, 0, 1, 1 ]);
-    }else {
-      let formData = {
+      setInputErrorArr([0, 0, 1, 0, 0]);
+    } else if (password !== confirmPassword) {
+      setMessage('Password confirmation do not Match');
+      setInputErrorArr([0, 0, 0, 1, 1]);
+    } else {
+      const formData = {
         user: {
           first_name: firstName,
           last_name: lastName,
-          email: email,
-          password: password,
+          email,
+          password,
           avatar: userAvatarFile,
-        }
+        },
       };
-       
+
       setMessage(null);
-      setInputErrorArr([0, 0, 0, 0, 0])
+      setInputErrorArr([0, 0, 0, 0, 0]);
       console.log(formData);
     }
   };
 
   return (
     <div className="w-full flex flex-col items-center">
-      <h2 className='authentication-title'>UserName Account</h2>
+      <h2 className="authentication-title">UserName Account</h2>
       <div className="user-auth-image-wrap" onClick={() => viewProfile(true)}>
         <img src={useImage} alt="" className="user-image-preview" />
       </div>
@@ -99,37 +98,37 @@ const UpdateAccount = (props) => {
         accept="image/*"
         id="user-avatar"
         className="user-authentication-input-file"
-        onChange={(e) =>{
+        onChange={(e) => {
           setUserImage(URL.createObjectURL(e.target.files[0]));
           setUserAvatarFile(e.target.files[0]);
         }}
       />
       <FormR
-        classForm={"user-authentication-form "}
+        classForm="user-authentication-form "
         inputsArray={inputToUpdate}
         submitFunction={onSignup}
-        submitButton={"Update Account"}
-        submitClass={"user-authentication-form-button"}
+        submitButton="Update Account"
+        submitClass="user-authentication-form-button"
         errorMessage={message}
-        inputErrorArr = {inputErrorArr}
+        inputErrorArr={inputErrorArr}
       />
 
-        <div className={profilePopUp ? "image-pop-up-container": "hidden"} >
-          <div className="w-full relative">
-            <button type="button" onClick={() => setProfilePopUp(false)}>
-              X
-            </button>
-          </div>
-         
-          <label htmlFor="user-avatar" >
-            <div className="image-pop-up-wrap">
-              <img src={useImage} alt="" className="image-pop-up" onClick={() => setProfilePopUp(false)}/>
-            </div> 
-          </label>
+      <div className={profilePopUp ? 'image-pop-up-container' : 'hidden'}>
+        <div className="w-full relative">
+          <button type="button" onClick={() => setProfilePopUp(false)}>
+            X
+          </button>
         </div>
-      
+
+        <label htmlFor="user-avatar">
+          <div className="image-pop-up-wrap">
+            <img src={useImage} alt="" className="image-pop-up" onClick={() => setProfilePopUp(false)} />
+          </div>
+        </label>
+      </div>
+
     </div>
   );
 };
 
-export default UpdateAccount
+export default UpdateAccount;
