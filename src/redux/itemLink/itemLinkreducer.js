e0ffdcc64282231da03caf9f4d12767c5e05dcbe
camilevahviraki <camilevahviraki@gmail.com>
2023-03-19 14:25:25 +0200

    Update item link reducer

--- src/redux/itemLink/itemLinkreducer.js
+++ src/redux/itemLink/itemLinkreducer.js
@@ -1,28 +1,29 @@
-const STORE_ITEM_LINK = "/redux/authentication/SET_ITEM_LINK";
+const STORE_ITEM_LINK = '/redux/authentication/SET_ITEM_LINK';
 
 const itemLinkReducer = (
-    state = {link: null}, action ) => {
-    switch (action.type) {
-      case STORE_ITEM_LINK: {
-        localStorage.setItem("storeItemLink", action.data.link);
-        localStorage.setItem('current-item-id', action.data.itemId);
-        return action.data;
-      }default: {
-        const link = localStorage.getItem('storeItemLink');
-        const itemId = localStorage.getItem('current-item-id');
-        if(link){
-            return {link: link, itemId: itemId};
-        }
-        return state;
+  state = { link: null }, action,
+) => {
+  switch (action.type) {
+    case STORE_ITEM_LINK: {
+      localStorage.setItem('storeItemLink', action.data.link);
+      localStorage.setItem('current-item-id', action.data.itemId);
+      return action.data;
+    } default: {
+      const link = localStorage.getItem('storeItemLink');
+      const itemId = localStorage.getItem('current-item-id');
+      if (link) {
+        return { link, itemId };
       }
+      return state;
     }
-}
+  }
+};
 
 export const setItemLink = (link, itemId) => (
-    {
-        type: STORE_ITEM_LINK,
-        data: {link, itemId},
-    }
-)
+  {
+    type: STORE_ITEM_LINK,
+    data: { link, itemId },
+  }
+);
 
 export default itemLinkReducer;
