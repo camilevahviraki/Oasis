ff40de9f7cb2b27cb99b1c5ce6c7796b7a4f1ff1
camilevahviraki <camilevahviraki@gmail.com>
2023-03-19 14:23:59 +0200

    Update home reducer

--- src/redux/home/homeReducer.js
+++ src/redux/home/homeReducer.js
@@ -1,27 +1,27 @@
-import axios from "axios";
-import linkURL from "../link";
+import axios from 'axios';
+import linkURL from '../link';
 
 const GET_HOME_ITEMS_LIST = 'redux/store/getStoresReducer/GET_HOME_ITEMS_LIST';
 
-
 const homeReducer = (state = [], action) => {
-    switch (action.type) {
-        case GET_HOME_ITEMS_LIST: {
-           return action.data;
-        }default:
-            return state
-    }
-}
+  switch (action.type) {
+    case GET_HOME_ITEMS_LIST: {
+      return action.data;
+    } default:
+      return state;
+  }
+};
 
 export const getHomeItems = (user, token) => (dispatch) => {
-    axios.get(`${linkURL}/user/user_params/home_page`)
-      .then((response) => {dispatch(
+  axios.get(`${linkURL}/user/user_params/home_page`)
+    .then((response) => {
+      dispatch(
         {
           type: GET_HOME_ITEMS_LIST,
           data: response.data,
         },
-      )
+      );
     });
-}
+};
 
 export default homeReducer;
