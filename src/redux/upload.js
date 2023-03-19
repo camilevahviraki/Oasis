3da66a8474e99f05ffbc37f89af6dae6b453cbd2
camilevahviraki <camilevahviraki@gmail.com>
2023-03-19 14:28:24 +0200

    Update upload function

--- src/redux/upload.js
+++ src/redux/upload.js
@@ -1,59 +1,55 @@
-import linkURL from './link';
 import axios from 'axios';
+import linkURL from './link';
 
 const Upload = (props) => {
-    
-    const {
-      endPoint,
-      data,
-      dispatchResponse,
-      getProgress,
-      dispatcthAuthResponse,
-      sendData,
-      method,
-      token,
-    } = props
+  const {
+    endPoint,
+    data,
+    dispatchResponse,
+    getProgress,
+    dispatcthAuthResponse,
+    sendData,
+    method,
+    token,
+  } = props;
 
-    let axiosMethod;
-    if(method){
-      axiosMethod = method;
-    }else{
-      axiosMethod = axios.post;
-    }
-    
-    axiosMethod(`${linkURL}/${endPoint}`, data,
+  let axiosMethod;
+  if (method) {
+    axiosMethod = method;
+  } else {
+    axiosMethod = axios.post;
+  }
+
+  axiosMethod(`${linkURL}/${endPoint}`, data,
     {
       headers: {
         // Authorization: `Bearer ${token}`,
-        'Content-Type': 'multipart/form-data'
+        'Content-Type': 'multipart/form-data',
       },
       onUploadProgress: (progressEvent) => {
         const progress = (progressEvent.loaded / progressEvent.total) * 100;
-        if(getProgress){
-          getProgress(progress)
+        if (getProgress) {
+          getProgress(progress);
         }
-      }
+      },
     }).then((response) => {
-      if(response.data.store_id && dispatchResponse){
-        dispatchResponse(response.data);
-      }
+    if (response.data.store_id && dispatchResponse) {
+      dispatchResponse(response.data);
+    }
 
-      if(dispatcthAuthResponse && response.data.token){
-        dispatcthAuthResponse(response.data);
-      }
+    if (dispatcthAuthResponse && response.data.token) {
+      dispatcthAuthResponse(response.data);
+    }
 
-      if(sendData){
-        sendData(response.data);
-      }
+    if (sendData) {
+      sendData(response.data);
+    }
 
-        console.log('response =>', response.data);
-      }).catch((err) => {
-        console.log('error =>', err);
-        if(sendData){
-          sendData('error');
-        }
-      })
-      
-}
+  }).catch((err) => {
+    if (sendData) {
+      sendData('error');
+    }
+  });
+};
 
-export default Upload
+export default Upload;
