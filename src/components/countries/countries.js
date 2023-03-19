557ba21a15bb453dca0da9b9a281b3c23e54054c
camilevahviraki <camilevahviraki@gmail.com>
2023-03-19 14:19:37 +0200

    Update components

--- src/components/countries/countries.js
+++ src/components/countries/countries.js
@@ -1,99 +1,98 @@
-import React,{useState} from 'react'; 
+import React, { useState } from 'react';
 import './countries.css';
 
 const Countries = (props) => {
-   const [inputValue, setInputValue] = useState('');
-   const [countriesShown, setCountriesShown] = useState([]);
-   const [listVisible, setListVisible] = useState(false);
-   const [selectedCountry, setSelectedCountry] = useState([]);
+  const [inputValue, setInputValue] = useState('');
+  const [countriesShown, setCountriesShown] = useState([]);
+  const [listVisible, setListVisible] = useState(false);
+  const [selectedCountry, setSelectedCountry] = useState([]);
 
-   const {
-     getSelectedCountry,
-     data,
-     inputClass,
-     inputWrapperClassName,
-     inputLabel,
-   } = props;
+  const {
+    getSelectedCountry,
+    data,
+    inputClass,
+    inputWrapperClassName,
+    inputLabel,
+    placeholder,
+  } = props;
 
-   document.addEventListener('mouseup', (e) => {
+  document.addEventListener('mouseup', (e) => {
     const container = document.getElementById('select-country');
     if (!container.contains(e.target)) {
-        setListVisible(false);
+      setListVisible(false);
     }
-   });
-   
+  });
+
   const countriesList = data;
 
   const searchCountry = (e) => {
-    const value = e.target.value;
+    const { value } = e.target;
 
-    const newlist = countriesList.filter((country) => 
-      country.name.toLowerCase().includes(value)
-    );
+    const newlist = countriesList.filter((country) => country.name.toLowerCase().includes(value));
     setInputValue(value);
     setCountriesShown(newlist);
     setListVisible(true);
 
     const country = countriesList.filter((country) => country.name === inputValue);
-    if(getSelectedCountry && country.length > 0){
-       getSelectedCountry(country[0]);
+    if (getSelectedCountry && country.length > 0) {
+      getSelectedCountry(country[0]);
     }
-  }
+  };
 
   const selectCountry = (country) => {
     setSelectedCountry(country);
     setInputValue(country.name);
     setListVisible(false);
-    if(getSelectedCountry){
+    if (getSelectedCountry) {
       getSelectedCountry(country);
     }
-  }
+  };
 
   return (
-    
+
     <div
-      id='select-country'
-      className={inputWrapperClassName ? inputWrapperClassName  : "formR-input-wrap"}
+      id="select-country"
+      className={inputWrapperClassName || 'formR-input-wrap'}
     >
-        {inputLabel ? (
-                    <label>{inputLabel}</label>
-                  ) : (
-                    <></>
-                  )}
-        <input
-          type='text'
-          value={inputValue}
-          onChange={searchCountry}
-          onClick={() => {
-            setListVisible(true)
-            if(inputValue === ''){
-              setCountriesShown(countriesList);
-            }
-          }} 
-          placeholder='search country'
-          className={inputClass? inputClass : 'country-search-input'}
-        />
-       {
-        listVisible?(
-            <div className='countries-list'>
-              {
+      {inputLabel ? (
+        <label>{inputLabel}</label>
+      ) : (
+        <></>
+      )}
+      <input
+        type="text"
+        value={inputValue}
+        onChange={searchCountry}
+        onClick={() => {
+          setListVisible(true);
+          if (inputValue === '') {
+            setCountriesShown(countriesList);
+          }
+        }}
+        placeholder={placeholder || 'search country'}
+        className={inputClass || 'country-search-input'}
+      />
+      {
+        listVisible ? (
+          <div className="countries-list">
+            {
                 countriesShown.map((country) => (
-                    <div
-                      className={country.name === selectedCountry.name? 'country-item selected-country':'country-item'}
-                      onClick={() => selectCountry(country)}
-                    >
-                        <p>
-                          {country.name}
-                        </p>
-                    </div>
+                  <div
+                    className={country.name === selectedCountry.name ? 'country-item selected-country' : 'country-item'}
+                    onClick={() => selectCountry(country)}
+                  >
+                    <p>
+                      {country.name}
+                    </p>
+                  </div>
                 ))
               }
-            </div>)
-            :
-            (<></>)
+          </div>
+        )
+          : (<></>)
        }
     </div>
-  )
-}
+  );
+};
 
-export default Countries
+export default Countries;
