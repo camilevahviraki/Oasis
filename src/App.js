4213e2357a617ff3701e29ac56bbe35644646fa1
camilevahviraki <camilevahviraki@gmail.com>
2023-03-19 14:16:18 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-import "./App.css";
-import { Provider, useSelector } from "react-redux";
-import store from "./redux/store";
-import CreateStore from "./seller/stores/CreateStore/CreateStore";
-import MainRouter from "./router/MainRouter";
+import './App.css';
+import { Provider, useSelector } from 'react-redux';
+import store from './redux/store';
+import CreateStore from './seller/stores/CreateStore/CreateStore';
+import MainRouter from './router/MainRouter';
 
 function App() {
   return (
     <Provider store={store}>
       <div className="App">
-        <MainRouter/>
+        <MainRouter />
       </div>
     </Provider>
   );
