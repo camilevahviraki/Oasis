8922a1e6e553d1503248b83b8256ddbe1da8342f
camilevahviraki <camilevahviraki@gmail.com>
2023-03-19 14:24:24 +0200

    Update item reducer

--- src/redux/item/itemShow.js
+++ src/redux/item/itemShow.js
@@ -1,27 +1,27 @@
-import axios from "axios";
-import linkURL from "../link";
+import axios from 'axios';
+import linkURL from '../link';
 
 const GET_SINGLE_ITEM = 'redux/store/getStoresReducer/GET_SINGLE_ITEM';
 
-
 const getItemDetails = (state = {}, action) => {
-    switch (action.type) {
-        case GET_SINGLE_ITEM: {
-           return action.data;
-        }default:
-            return state;
-    }
-} 
+  switch (action.type) {
+    case GET_SINGLE_ITEM: {
+      return action.data;
+    } default:
+      return state;
+  }
+};
 
 export const getItem = (item, token) => (dispatch) => {
-    axios.get(`${linkURL}/api_stores/${item.store_id}/item/${item.id}`)
-      .then((response) => {dispatch(
+  axios.get(`${linkURL}/api_stores/${item.store_id}/item/${item.id}`)
+    .then((response) => {
+      dispatch(
         {
           type: GET_SINGLE_ITEM,
           data: response.data,
         },
-      )
+      );
     });
-}
+};
 
 export default getItemDetails;
