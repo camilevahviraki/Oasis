fc29189ab729b26d6ff1f2498b4ae88ecf2c0711
camilevahviraki <camilevahviraki@gmail.com>
2023-03-19 14:20:04 +0200

    Update header

--- src/header/Header.js
+++ src/header/Header.js
@@ -17,56 +17,56 @@ import './Header.css';
 const Header = () => {
   const [showMenu, setShowMenu] = useState(false);
   const [searchValue, setSearchValue] = useState('');
-  const userData = useSelector(state => state.authenticationReducer);
-  const userNames = userData.user.first_name + '-' + userData.user.last_name;
+  const userData = useSelector((state) => state.authenticationReducer);
+  const userNames = `${userData.user.first_name}-${userData.user.last_name}`;
   const userImage = userData.user.avatar_url;
   const hideMenu = () => {
     setShowMenu(false);
-  }
+  };
   const links = [
-    {id: 1, icon: homeIcon, link: '../'},
-    {id: 2, icon: placesIcon, link: '../places'},
-    {id: 3, icon: storeIcon, link: '../my-store'},
-  ]
+    { id: 1, icon: homeIcon, link: '../' },
+    { id: 2, icon: placesIcon, link: '../places' },
+    { id: 3, icon: storeIcon, link: '../my-store' },
+  ];
 
   const changeSearchValue = (e) => {
     setSearchValue(e.target.value);
-  }
+  };
   return (
-    <header className='flex align-center w-full'>
-      <div className='logo-wrap'>
+    <header className="flex align-center w-full">
+      <div className="logo-wrap">
         <h7>OASIS</h7>
       </div>
-       <div className='flex align-center header-icons'>
-          {links.map((link) => (
-            <Link to={link.link}>
-              {link.icon?<img src={link.icon} alt=''/>:'link'}
-            </Link>
-          ))}
-       </div>
-       <div className='right-icons-wrap flex align-center'>
+      <div className="flex align-center header-icons">
+        {links.map((link) => (
+          <Link to={link.link}>
+            {link.icon ? <img src={link.icon} alt="" /> : 'link'}
+          </Link>
+        ))}
+      </div>
+      <div className="right-icons-wrap flex align-center">
         {/* ////////////////// Currencies */}
-        <Currency/>
+        <Currency />
 
-        <Link to='cart'>
-            <img src={cartIcon} alt='' className='user-Icon'/>
+        <Link to="cart">
+          <img src={cartIcon} alt="" className="user-Icon" />
         </Link>
-        <Link to='chat'>
-          <img src={chatIcon} alt='' className='user-Icon'/>
+        <Link to="chat">
+          <img src={chatIcon} alt="" className="user-Icon" />
         </Link>
         <Link to={`../account/${linkName(userNames)}`}>
-          <div className='user-Icon-wrap'>
-            <img src={CheckValidImage({avartarUrl: userImage,defaultImg: userIcon})} alt='' className='user-Icon-r'/>
+          <div className="user-Icon-wrap">
+            <img src={CheckValidImage({ avartarUrl: userImage, defaultImg: userIcon })} alt="" className="user-Icon-r" />
           </div>
         </Link>
-        <button type='button' onClick={() => setShowMenu(true)}>
-          <img src={hamburger} alt='' className='user-Icon'/>
+        <button type="button" onClick={() => setShowMenu(true)}>
+          <img src={hamburger} alt="" className="user-Icon" />
         </button>
-       </div>
-       {showMenu? <Menu hideMenu = {hideMenu}/>:<></>}
-       
-    </header>
-  )
-}
+      </div>
+      {showMenu ? <Menu hideMenu={hideMenu} /> : <></>}
 
-export default Header
+    </header>
+  );
+};
+
+export default Header;
--- src/header/currencies/Currency.js
+++ src/header/currencies/Currency.js
@@ -1,22 +1,22 @@
-import React, {useEffect} from 'react';
+import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { getCurrencies } from '../../redux/currencies/currenciesReducer';
 
 const Currency = () => {
   const dispatch = useDispatch();
-  const currencies = useSelector(state => state.currenciesReducer);  
+  const currencies = useSelector((state) => state.currenciesReducer);
 
   useEffect(() => {
     dispatch(getCurrencies());
   }, []);
 
-  console.log('currencies =>' ,currencies)
+  console.log('currencies =>', currencies);
 
   return (
-    <div style={{border: '1px solid green', padding: '6px'}}>
-        USD $
+    <div style={{ border: '1px solid green', padding: '6px' }}>
+      USD $
     </div>
-  )
-}
+  );
+};
 
 export default Currency;
