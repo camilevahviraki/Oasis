4d90159ff85d22326d675dc1e8b64a503b3ca607
camilevahviraki <camilevahviraki@gmail.com>
2023-03-19 14:18:03 +0200

    Update home.js

--- src/client/home/Home.js
+++ src/client/home/Home.js
@@ -3,16 +3,17 @@ import { useSelector, useDispatch } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { setItemLink } from '../../redux/itemLink/itemLinkreducer';
 import { getHomeItems } from '../../redux/home/homeReducer';
+import { clearStoreData } from '../../redux/stores/getStoreShowReducer';
 import ItemsList from '../../seller/items/itemList/ItemIndex';
 import SearchBar from '../../reusable/serach-bar/SearchBar';
 import './Home.css';
 
 const Home = () => {
-
   const dispatch = useDispatch();
 
   useEffect(() => {
-    dispatch(getHomeItems())
+    // dispatch(clearStoreData());
+    dispatch(getHomeItems());
   }, []);
 
   const data = useSelector((state) => state.homeReducer);
@@ -22,11 +23,11 @@ const Home = () => {
   };
 
   return (
-    <div className='home-container'>
-      <SearchBar/>
-      <ItemsList itemsData={data}/>
+    <div className="home-container">
+      <SearchBar />
+      <ItemsList itemsData={data} />
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
