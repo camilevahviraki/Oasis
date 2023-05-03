aef79d252d7fc9c46551b671a3a37d0ba5ed5645
camilevahviraki <camilevahviraki@gmail.com>
2023-05-04 00:03:51 +0200

    Add currencies reducer

---
+++ src/redux/currencies/selectedCurrency.js
@@ -0,0 +1,32 @@
+const SELECT_CURRENCY = "redux/store/getStoresReducer/SELECT_CURRENCY";
+
+const selectedCurrency = (
+  state = {
+    country: "United States",
+    exchange: 1000,
+    name: "USD",
+    symbole: "$",
+  },
+  action
+) => {
+  switch (action.type) {
+    case SELECT_CURRENCY: {
+      localStorage.setItem("selectedCurrency", JSON.stringify(action.data));
+      return action.data;
+    }
+    default: {
+      const savedCurrency = localStorage.getItem("selectedCurrency");
+      if (savedCurrency) {
+        return JSON.parse(savedCurrency);
+      }
+      return state;
+    }
+  }
+};
+
+export const setCurrency = (currency) => ({
+  type: SELECT_CURRENCY,
+  data: currency,
+});
+
+export default selectedCurrency;
