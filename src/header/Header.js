e99b4417e22fcf5414da7090c9af32716a08a7c0
camilevahviraki <camilevahviraki@gmail.com>
2023-05-04 00:01:40 +0200

    Update header

--- src/header/Header.js
+++ src/header/Header.js
@@ -1,6 +1,9 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
+import {HiOutlineShoppingCart} from 'react-icons/hi';
+import {RiMessengerLine} from 'react-icons/ri';
+import {getCartItems} from '../redux/cart/getCartsItemReducer';
 import Menu from './menu';
 import linkName from '../reusable/remove-blanck-space/linkName';
 import Currency from './currencies/Currency';
@@ -9,12 +12,12 @@ import chatIcon from '../images/icons/chat-icon-f.png';
 import placesIcon from '../images/icons/travel_explore_FILL0_wght400_GRAD0_opsz48.png';
 import homeIcon from '../images/home-icon.png';
 import userIcon from '../images/user-icon.png';
-import cartIcon from '../images/icons/shopping_cart_FILL0_wght400_GRAD0_opsz48.png';
 import hamburger from '../images/icons/menu-hamburger.png';
 import storeIcon from '../images/icons/storefront_FILL0_wght400_GRAD0_opsz48.png';
 import './Header.css';
 
 const Header = () => {
+  const dispatch = useDispatch();
   const [showMenu, setShowMenu] = useState(false);
   const [searchValue, setSearchValue] = useState('');
   const userData = useSelector((state) => state.authenticationReducer);
@@ -23,6 +26,13 @@ const Header = () => {
   const hideMenu = () => {
     setShowMenu(false);
   };
+
+  useEffect(() => {
+    dispatch(getCartItems(userData.user.id))
+   }, []);
+   
+  const cartData = useSelector(state => state.cartItemsReducer); 
+
   const links = [
     { id: 1, icon: homeIcon, link: '../' },
     { id: 2, icon: placesIcon, link: '../places' },
@@ -32,6 +42,7 @@ const Header = () => {
   const changeSearchValue = (e) => {
     setSearchValue(e.target.value);
   };
+
   return (
     <header className="flex align-center w-full">
       <div className="logo-wrap">
@@ -45,14 +56,14 @@ const Header = () => {
         ))}
       </div>
       <div className="right-icons-wrap flex align-center">
-        {/* ////////////////// Currencies */}
         <Currency />
 
-        <Link to="cart">
-          <img src={cartIcon} alt="" className="user-Icon" />
+        <Link to="../cart" className='cart-icon'>
+        <span className='header-cart-length'>{cartData.length}</span>
+          <HiOutlineShoppingCart className="user-Icon"/>
         </Link>
-        <Link to="chat">
-          <img src={chatIcon} alt="" className="user-Icon" />
+        <Link to="../chat" >
+          <RiMessengerLine className="user-Icon"/>
         </Link>
         <Link to={`../account/${linkName(userNames)}`}>
           <div className="user-Icon-wrap">
--- src/header/currencies/Currency.js
+++ src/header/currencies/Currency.js
@@ -1,20 +1,57 @@
-import React, { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { getCurrencies } from '../../redux/currencies/currenciesReducer';
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getCurrencies } from "../../redux/currencies/currenciesReducer";
+import { setCurrency } from "../../redux/currencies/selectedCurrency";
+import './Currency.css';
 
 const Currency = () => {
   const dispatch = useDispatch();
   const currencies = useSelector((state) => state.currenciesReducer);
+  const [showCurrencyList, setCurrencyList] = useState(false);
+  const selectedCurrency = useSelector(state => state.selectedCurrency);
 
   useEffect(() => {
     dispatch(getCurrencies());
   }, []);
 
-  console.log('currencies =>', currencies);
+  const selectCurrency = (currency) => {
+     setCurrencyList(false);
+     dispatch(setCurrency(currency));
+  } 
+
+  console.log("currencies =>", currencies);
 
   return (
-    <div style={{ border: '1px solid green', padding: '6px' }}>
-      USD $
+    <div
+      className="currencies_container"
+      style={{ border: "1px solid green", padding: "6px" }}
+      onClick={() => setCurrencyList(!showCurrencyList)}
+    >
+      <div className="selected-currency">
+        {currencies.length > 0? 
+        (<>
+        {selectedCurrency.name}{' '}{selectedCurrency.symbole}
+        </>)
+        :(<></>)}
+      </div>
+      {
+        showCurrencyList?
+        (<div className="currencies-list">
+        {currencies.map((currency) => {
+          const { country, exchange, id, name, symbole } = currency;
+          return (
+            <div
+              className={name === selectedCurrency.name ? "currency-wrapp selected-currency-in-list":"currency-wrapp"}
+              onClick={() => selectCurrency(currency)}
+            >
+              <span className="currency-symbol">{symbole}{' '}</span>
+              <span>{name}{' '}</span>
+            </div>
+          )
+        })}
+      </div>):
+      (<></>)
+      }
     </div>
   );
 };
