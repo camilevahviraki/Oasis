52fe874be12a561f38b8fc8e7a152503f733d0e2
camilevahviraki <camilevahviraki@gmail.com>
2023-05-04 00:02:08 +0200

    Update item reducers

--- src/redux/item/getItem.js
+++ src/redux/item/getItem.js
@@ -2,22 +2,53 @@ import axios from 'axios';
 import linkURL from '../link';
 
 const GET_ITEMS_LIST = 'redux/store/getStoresReducer/GET_ITEMS_LIST';
+const SEARCH_STORE_ITEM = 'redux/store/getStoresReducer/SEARCH_STORE_ITEM';
+const DELETE_SEARCHED_DATA = 'redux/store/getStoresReducer/DELETE_SEARCHED_DATA';
 
-const getItemsList = (state = [], action) => {
+const getItemsList = (state = {itemsList: []}, action) => {
   switch (action.type) {
     case GET_ITEMS_LIST: {
-      return action.data;
-    } default:
+      return {itemsList: action.data};
+    }case SEARCH_STORE_ITEM: {
+      return {itemsList: action.data, searchedData: true};
+    }case DELETE_SEARCHED_DATA: {
+      return {itemsList: state.itemsList, searchedData: null};
+    }default:
       return state;
   }
 };
 
-export const getItems = (store, token) => (dispatch) => {
-  axios.get(`${linkURL}/api_stores/${store.store_id}/items/${store.category}`)
+export const deleteSearchedData = () => ({
+  type: DELETE_SEARCHED_DATA,
+})
+
+export const getItems = (data, token) => (dispatch) => {
+  const {
+    store_id,
+    category,
+  } = data;
+  axios.get(`${linkURL}/api_stores/${store_id}/items/${category}`)
     .then((response) => {
       dispatch(
         {
           type: GET_ITEMS_LIST,
+          data: response.data,
+        },
+      );
+    });
+};
+
+export const searchStoreItem = (data, token) => (dispatch) => {
+  const {
+    query,
+    storeId,
+    categoryName,
+  } = data;
+  axios.post(`${linkURL}/api_stores/${storeId}/items/${categoryName}/search`, {query})
+    .then((response) => {
+      dispatch(
+        {
+          type: SEARCH_STORE_ITEM,
           data: response.data,
         },
       );
