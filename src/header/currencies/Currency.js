c12f9978a3e0ae082aeaa483917e37e53b69db09
camilevahviraki <camilevahviraki@gmail.com>
2023-05-25 02:17:46 +0200

    Update Currencies component

--- src/header/currencies/Currency.js
+++ src/header/currencies/Currency.js
@@ -1,57 +1,67 @@
 import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import { AiOutlineDown, AiOutlineRight } from "react-icons/ai";
 import { getCurrencies } from "../../redux/currencies/currenciesReducer";
 import { setCurrency } from "../../redux/currencies/selectedCurrency";
-import './Currency.css';
+import "./Currency.css";
 
 const Currency = () => {
   const dispatch = useDispatch();
   const currencies = useSelector((state) => state.currenciesReducer);
   const [showCurrencyList, setCurrencyList] = useState(false);
-  const selectedCurrency = useSelector(state => state.selectedCurrency);
+  const selectedCurrency = useSelector((state) => state.selectedCurrency);
 
   useEffect(() => {
     dispatch(getCurrencies());
   }, []);
 
   const selectCurrency = (currency) => {
-     setCurrencyList(false);
-     dispatch(setCurrency(currency));
-  } 
-
-  console.log("currencies =>", currencies);
+    setCurrencyList(false);
+    dispatch(setCurrency(currency));
+  };
 
   return (
     <div
       className="currencies_container"
-      style={{ border: "1px solid green", padding: "6px" }}
       onClick={() => setCurrencyList(!showCurrencyList)}
     >
       <div className="selected-currency">
-        {currencies.length > 0? 
-        (<>
-        {selectedCurrency.name}{' '}{selectedCurrency.symbole}
-        </>)
-        :(<></>)}
+        {currencies.length > 0 ? (
+          <>
+            <span>{selectedCurrency.name}</span> {selectedCurrency.symbole}
+            <AiOutlineDown
+              className={
+                showCurrencyList ? "currency-arrow-turned" : "currency-arrow"
+              }
+              color="#707070"
+            />
+          </>
+        ) : (
+          <></>
+        )}
       </div>
-      {
-        showCurrencyList?
-        (<div className="currencies-list">
-        {currencies.map((currency) => {
-          const { country, exchange, id, name, symbole } = currency;
-          return (
-            <div
-              className={name === selectedCurrency.name ? "currency-wrapp selected-currency-in-list":"currency-wrapp"}
-              onClick={() => selectCurrency(currency)}
-            >
-              <span className="currency-symbol">{symbole}{' '}</span>
-              <span>{name}{' '}</span>
-            </div>
-          )
-        })}
-      </div>):
-      (<></>)
-      }
+      {showCurrencyList ? (
+        <div className="currencies-list">
+          {currencies.map((currency) => {
+            const { _country, name, symbole } = currency;
+            return (
+              <div
+                className={
+                  name === selectedCurrency.name
+                    ? "currency-wrapp selected-currency-in-list"
+                    : "currency-wrapp"
+                }
+                onClick={() => selectCurrency(currency)}
+              >
+                <span className="currency-symbol">{symbole} </span>
+                <span>{name} </span>
+              </div>
+            );
+          })}
+        </div>
+      ) : (
+        <></>
+      )}
     </div>
   );
 };
