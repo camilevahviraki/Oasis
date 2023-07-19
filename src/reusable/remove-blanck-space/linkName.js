857b2d721e525fa25493cf7d95d1079a73f3b01e
camilevahviraki <camilevahviraki@gmail.com>
2023-07-20 00:20:34 +0200

    Update reusable

--- src/reusable/remove-blanck-space/linkName.js
+++ src/reusable/remove-blanck-space/linkName.js
@@ -1,5 +1,9 @@
 const linkName = (string) => {
-  const stringArr = string.split('');
+  let string2 = '';
+  if (string) {
+    string2 = string;
+  }
+  const stringArr = string2.split('');
   let newStringArr = [];
 
   stringArr.forEach((elmnt) => {
